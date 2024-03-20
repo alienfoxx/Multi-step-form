ab612f001132a7f875e8e43a6eea84797f359e7a
alienfoxx <alienfoxx001@gmail.com>
2024-03-20 02:13:01 +0000

    steup the 2 steps

--- script.js
+++ script.js
@@ -6,40 +6,15 @@ const myInputs= document.querySelectorAll('input');
  const inputBox= document.getElementsByClassName('input-field');
 const warningMsgs = document.querySelectorAll('h5')
 
-
-
-// requireNot.style.cssText= 'color: red; font-weight:700';
-
-
-
 function checkInput() {
-
     myInputs.forEach(input => {
         let isEmpty =false;
         if (input.value === '') {
-            input.style.borderColor = 'red';
-            isEmpty =true;
-            warningMsgs.forEach(warningmsg => {
-                if(myInputs = ''){
-                    
-                }
-            })
-            
+            input.style.borderColor = 'red';   
         } else {
             input.style.borderColor = 'hsl(206, 94%, 87%)';
         }
-       
     });
-
-    function displayingNsg(){
-       
-    }
-    
-
-    // its still need i ' message displaying to tell user thats the field is required'
-    
-}
-function checkValidation(){
-
+    // its still need i ' message displaying to tell user thats the field is required' 
 }
 
